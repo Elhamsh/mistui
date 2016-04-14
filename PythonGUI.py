5924ff5c7026b187f2631df5e09be72253c42a14
markravi <markandayravi@gmail.com>
2016-04-14 16:19:07 -0700

    change

--- PythonGUI.py
+++ PythonGUI.py
@@ -20,14 +20,10 @@ class simpleGUI_tk(tk.Tk):
         self.list()
         self.button = tk.Button(self.parent, text = "Output Map")
 
-
-    def openFile(self):
-        print("")
-        #
-    def Preprocess(self):
+    def Preprocess(self,event):
         obj.AtomRepDistDRT()
 
-    def runFinderProgram(self):
+    def runFinderProgram(self,event):
         print("")
         cases= [21.0, 24.0, 42.0, 53.0, 68.0, 85.0, 107.0, 110.0, 112.0, 166.0, 177.0, 211.0, 218.0, 219.0, 231.0, 234.0, 239.0, 240.0,
         241.0, 248.0, 251.0, 253.0, 256.0, 258.0, 265.0, 274.0, 276.0,279.0,280.0,283.0,286.0,287.0]
@@ -41,7 +37,7 @@ class simpleGUI_tk(tk.Tk):
         #call elham's code
 
 
-    def drawOutput(self):
+    def drawOutput(self,event):
         try:
             dm.createTest(292)
         except:
@@ -51,11 +47,11 @@ class simpleGUI_tk(tk.Tk):
         #C:\Users\eshaaban\PycharmProjects\MISTUI\Output\292.html
         webbrowser.open_new_tab(filename)
 
-    def aboutFinder(self):
+    def aboutFinder(self,event):
         print("here is the explanation of what the finder does, add extra stuff")
 
 
-    def helpFinder(self):
+    def helpFinder(self,event):
         window = tk.Toplevel(root)
         window.minsize(400,300);
         window.title("Help")
@@ -63,7 +59,8 @@ class simpleGUI_tk(tk.Tk):
                       "\n3. Press the 'Run' button under the 'Preprocess' tab\n" + \
                       "4. Under 'Draw', press 'Visualize Input' in order to see the inputs (reported locations) displayed on a map\n" +\
                       "5. Otherwise, press 'Search Area' to display the output onto the map after the input has been processed.\n"+\
-                      "6. Lastly, 'Exit'quits out of the program\n\n" + "Shortcuts: \nPreprocess: CTRL + P\nRun: CTRL + R\nExit: CTRL + E\nVisualize Input: CTRL + I\nSearch Area: CTRL + A\n"
+                      "6. Lastly, 'Exit'quits out of the program\n\n" + "Shortcuts: \nPreprocess: CTRL + P\nRun: CTRL + R\nExit: CTRL + " \
+                                                                        "E\nVisualize Input: CTRL + I\nSearch Area: CTRL + S\nHelp: CTRL + H\nAbout: CTRL + A"
         msg = Message(window, text=helpMessage)
         msg.pack()
 
@@ -76,20 +73,25 @@ class simpleGUI_tk(tk.Tk):
         # create a pulldown menu, and add it to the menu bar
         filemenu = tk.Menu(menubar, tearoff=0)
         # filemenu.add_command(label="Open Test Case", command=self.openFile)
-        filemenu.add_command(label='Preporcess', command=self.Preprocess)
-        filemenu.add_command(label="Run", command=self.runFinderProgram)
-
+        filemenu.add_command(label='Preprocess', command=self.Preprocess, accelerator="ctrl + p")
+        self.bind("<Control-p>", self.Preprocess)
+        filemenu.add_command(label="Run", command=self.runFinderProgram, accelerator="Ctrl + R")
+        self.bind("<Control-r>", self.runFinderProgram)
         drawMenu = Menu(filemenu)
-        drawMenu.add_command(label='Visualize Input')
-        drawMenu.add_command(label='Search Area', command=self.drawOutput)
+        drawMenu.add_command(label='Visualize Input', accelerator="Ctrl + I")
+        self.bind("<Control-I>")
+        drawMenu.add_command(label='Search Area', command=self.drawOutput, accelerator="Ctrl + S")
+        self.bind("<Control-s>", self.drawOutput)
         filemenu.add_cascade(label="Draw", menu=drawMenu)
         filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=self.quit)
-
+        filemenu.add_command(label="Exit", command=self.quit, accelerator="Ctrl + E")
+        self.bind("<Control-e>", self.quit)
         menubar.add_cascade(label="File", menu=filemenu)
         helpmenu = tk.Menu(menubar, tearoff=0)
-        helpmenu.add_command(label="Help", command=self.helpFinder)
-        helpmenu.add_command(label="About", command=self.aboutFinder)
+        helpmenu.add_command(label="Help", command=self.helpFinder, accelerator="Ctrl + H")
+        self.bind("<Control-h>", self.helpFinder)
+        helpmenu.add_command(label="About", command=self.aboutFinder, accelerator="Ctrl + A")
+        self.bind("<Control-a>", self.aboutFinder)
         menubar.add_cascade(label="Help", menu=helpmenu)
 
         # modelMenu = tk.Menu(menubar, tearoff = 0)
